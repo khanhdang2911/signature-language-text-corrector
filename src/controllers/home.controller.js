c1a9720e3fa8372b4ec73ede157aea17c8ec9548
Dang Xuan Khanh <khanhdang3152@gmail.com>
2024-09-08 20:04:24 +0700

    update/delete text voice from histories

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -44,4 +44,57 @@ const createTextVoice = async (req, res) => {
 		})
 	}
 }
-export { getAllHistory, createTextVoice }
+
+const deleteTextVoice = async (req, res) => {
+	try {
+		const id = req.params.id
+		if (!id) {
+			return res.status(404).json({
+				success: false,
+				message: 'Missing required parameters!',
+			})
+		}
+		await connection.query('delete from histories where id=?', [id])
+		return res.status(200).json({
+			success: true,
+			message: 'Text voice is deleted',
+		})
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			message: error,
+		})
+	}
+}
+
+const updateTextVoice = async (req, res) => {
+	try {
+		const data = req.body
+		if (!data.id || !data.text_voice) {
+			return res.status(404).json({
+				success: false,
+				message: 'Data is invalid from body!',
+			})
+		}
+		const currentDate = new Date()
+		const year = currentDate.getFullYear()
+		const month = currentDate.getMonth() + 1
+		const date = currentDate.getDate()
+		const hour = currentDate.getHours()
+		const minute = currentDate.getMinutes()
+		const second = currentDate.getSeconds()
+		const formatDate = `${year}-${month}-${date} ${hour}:${minute}:${second}`
+
+		await connection.query('update histories set text_voice=?, date_insert=? where id=? ', [data.text_voice, formatDate, data.id])
+		return res.status(200).json({
+			success: true,
+			message: 'Update text voice in histories successfully!',
+		})
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			message: error,
+		})
+	}
+}
+export { getAllHistory, createTextVoice, deleteTextVoice, updateTextVoice }
--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -6,5 +6,6 @@ const homeRouter = express.Router()
 
 homeRouter.get('/get-all-history', homeController.getAllHistory)
 homeRouter.post('/create-text-voice', homeController.createTextVoice)
-
+homeRouter.delete('/delete-text-voice/:id', homeController.deleteTextVoice)
+homeRouter.put('/update-text-voice',homeController.updateTextVoice)
 export default homeRouter
